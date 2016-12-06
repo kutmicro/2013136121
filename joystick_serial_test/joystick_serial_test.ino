43f515659420cc9dbb2febebea6714286bd0ec58
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-12-06 12:20:10 +0900

    joystick_serial_test

---
+++ joystick_serial_test/joystick_serial_test.ino
@@ -0,0 +1,24 @@
+int joystick_x = 1;    
+int joystick_y = 2;
+int joystick_z = 3;    //조이스틱 X,Y,Z축을 저장
+
+void setup()
+{
+ pinMode(joystick_z,INPUT);    // Z를 입력모드로
+ Serial.begin(9600);               // 시리얼 통신을, 9600속도로 받습니다. (숫자 조정은 자유)
+}
+
+void loop()
+{
+ int x = analogRead(joystick_x);      // 조이스틱 X축의 아날로그 신호를 x에 저장
+ int y = analogRead(joystick_y);      // 조이스틱 Y축의 아날로그 신호를 y에 저장
+ int z = analogRead(joystick_z);     // 조이스틱 Z축의 아날로그 신호를 z에 저장 
+  Serial.print("X: ");                   
+  Serial.print(x);                          //시리얼 모니터에 'X: x값'을 표기
+  Serial.print("  Y: ");
+  Serial.print(y);                          //시리얼 모니터에 '  Y: y값'을 표기
+  Serial.print("  Z: ");
+ Serial.println(z);                         //시리얼 모니터에 '  Z: z값'을 표기하고 줄넘김
+delay(100);                                  // 100만큼 지연
+} 
+//출처] 조이스틱모듈을 사용하기 (아두이노 강좌)|작성자 오픈랩
